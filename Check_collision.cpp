a82a4ce99a80c8065de59c39b66217ea5bb6833d
William A <76547127+TANK1-41@users.noreply.github.com>
2022-02-01 18:40:48 -0500

    merged collision to main branch removed unnecessary comments

--- Check_collision.cpp
+++ Check_collision.cpp
@@ -6,14 +6,6 @@
 
 
 void collision::checkCollision(std::vector<astroidMovment> *movementSlope, player *pla) {
-/* int leftA, leftB;
- int rightA, rightB;
-int topA, topB;
-int bottomA, bottomB;
-    leftA = pla.getDestRect().x;
-    rightA = pla.getDestRect().x + pla.getDestRect().w;
-    topA = pla.getDestRect().y;
-    bottomA = pla.getDestRect().y + pla.getDestRect().h;*/
 
     for (size_t i{0}; i < movementSlope->size(); i++) {
 
@@ -24,42 +16,6 @@ int bottomA, bottomB;
             i--;
         }
     }
-
-    /*leftB = movementSlope.at(i).instance->getDestRect().x;
-    rightB = movementSlope.at(i).instance->getDestRect().x + movementSlope.at(i).instance->getDestRect().w;
-    topB = movementSlope.at(i).instance->getDestRect().y;
-    bottomB = movementSlope.at(i).instance->getDestRect().y + movementSlope.at(i).instance->getDestRect().h;
-    if (bottomA <= topB) {
-        movementSlope.erase(movementSlope.begin() + i);
-        std::cout<<"astroid destroyed"<<std::endl;
-        i--;
-        break;
-    }
-
-    if (topA >= bottomB) {
-        movementSlope.erase(movementSlope.begin() + i);
-        std::cout<<"astroid destroyed"<<std::endl;
-        i--;
-        break;
-    }
-
-    if (rightA <= leftB) {
-        movementSlope.erase(movementSlope.begin() + i);
-        std::cout<<"astroid destroyed"<<std::endl;
-
-        i--;
-        break;
-    }
-
-    if (leftA >= rightB) {
-        movementSlope.erase(movementSlope.begin() + i);
-        std::cout<<"astroid destroyed"<<std::endl;
-
-        i--;
-        break;
-    }
-}*/
-
 }
 
 
